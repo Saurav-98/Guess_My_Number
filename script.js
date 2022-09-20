9985c7a4554e3faa6b8eff2c4677a37528853420
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-20 20:37:19 +0530

    Dom Selectors and handling event listeners

--- script.js
+++ script.js
@@ -9,3 +9,15 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+
+const guessEl = document.querySelector('.guess');
+const numberEl = document.querySelector('.message');
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(guessEl.value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    numberEl.textContent = '🚫 No Number';
+  }
+});
