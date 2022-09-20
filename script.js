cbd5fb88c5076d3090016d11e99e757025bf327a
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-09-20 21:18:14 +0530

    Working on Game Logic - 2

--- script.js
+++ script.js
@@ -16,21 +16,46 @@ const guessEl = document.querySelector('.guess');
 const messageEl = document.querySelector('.message');
 const numberEl = document.querySelector('.number');
 const scoreEl = document.querySelector('.score');
-// Defining the Secret Number
+const againBtn = document.querySelector('.again');
 
-const secretNumber = Math.trunc(Math.random() * 10 + 1);
+// Defining the Secret Number and Score
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+
+//  Making the Game reset Button Work
+
+againBtn.addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  messageEl.textContent = 'Start guessing...';
+  scoreEl.textContent = '20';
+  guessEl.value = '';
+  numberEl.textContent = '?';
+  document.body.style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+// Checking the Guess Value
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(guessEl.value);
   console.log(guess, typeof guess);
-  numberEl.textContent = secretNumber;
 
+  // Check for Invalid / missing input
   if (!guess) {
     messageEl.textContent = '🚫 No Number';
-  } else if (guess === secretNumber) {
+  }
+
+  // Check for Win Condition
+  else if (guess === secretNumber) {
     messageEl.textContent = '🎊🥳  Correct Number!';
-  } else if (guess > secretNumber) {
+    numberEl.textContent = secretNumber;
+    document.body.style.backgroundColor = '#60dd47';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  // Check for High Condition
+  else if (guess > secretNumber) {
     if (score > 0) {
       messageEl.textContent = '📈 Umm... Too High';
       score--;
@@ -39,7 +64,9 @@ document.querySelector('.check').addEventListener('click', function () {
       messageEl.textContent = '💀 Game Over!';
       scoreEl.textContent = score;
     }
-  } else if (guess < secretNumber) {
+  }
+  // Check for High Condition
+  else if (guess < secretNumber) {
     if (score > 0) {
       messageEl.textContent = '📉 Hmmm... Too Low';
       score--;
@@ -49,6 +76,4 @@ document.querySelector('.check').addEventListener('click', function () {
       scoreEl.textContent = score;
     }
   }
-
-  // .textContent = 40;
 });
